090bbaadf752077921a754d609b7b2e570dd1cab
MaximKirsch <Maximkirsch2003@hotmail.com>
2024-05-28 15:00:47 +0200

    implement on-empty-db

--- backend/db-config.js
+++ backend/db-config.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
-const mysql = require('mysql');
-const conPool = mysql.createPool({
+var mysql = require('mysql2');
+var conPool = mysql.createPool({
   connectionLimit : 10,
-  database: process.env.MYSQL_HOST_DATABASE, // sensible Daten wurden in die .env Datei ausgelagert
+  database: process.env.MYSQL_DATABASE, // sensible Daten wurden in die .env Datei ausgelagert
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_HOST_USER,
-  password: process.env.MYSQL_HOST_PASSWORD
-})
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD
+});
 
 module.exports = conPool;
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const initializeDbImp       = require("./router/on-empty-db");
+const socket       = require('socket.io');
 const path = require('path');
 const app = express();
 
@@ -7,3 +9,21 @@ app.use(express.static(path.join(__dirname, '../dist/hottake-db-web')));
 let server = app.listen('8020', function (){
   console.log("[Server Initialized on Port 8020]")
 })
+
+let ioSocket = socket(server);
+console.log("[Socket Initialized]")
+
+ioSocket.on('connection', (socket) => {
+  console.log(`Neue Socket-Verbindung: ${socket.id}`);
+  // exportiert den socket um diesen in der router.js zu verwenden
+  module.exports.ioSocket = ioSocket;
+
+  // gibt aus wenn eine Socket Verbindung aufgehoben/getrennt wurde
+  socket.on('disconnect', () => {
+    console.log(`Socket-Verbindung getrennt: ${socket.id}`);
+  });
+});
+
+initializeDbImp.initDb()
+
+
