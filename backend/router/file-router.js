39de1a3a1ed9ea29f89bca5882dad89935ccea56
MaximKirsch <Maximkirsch2003@hotmail.com>
2024-06-02 20:03:20 +0200

    Add Download File to Router

--- backend/router/file-router.js
+++ backend/router/file-router.js
@@ -33,6 +33,23 @@ router.get('/file', async (req, res) => {
     });
 });
 
+router.get('/file/:id/download', async (req, res) => {
+  const query = 'SELECT * FROM file WHERE id = ?';
+  const params = [req.params.id];
+
+  db.query(query, params, (err, results) => {
+    if (err) {
+      console.error('Database error:', err); // Fehler Protokoll
+      return res.status(500).send({message: "Server error."});
+    }
+    if (results.length > 0) {
+      res.status(200).set({'Content-Type': results[0].mimetype, 'Content-Disposition': `attachment; filename=${results[0].filename}`,}).end(results[0].data);   // vllt keine eckige
+    } else {
+      res.status(404).send({message: "file not found."});
+    }
+  });
+});
+
 //finde einen bestimmten user
 router.get('/file/:id', async (req, res) => {
 
